a0e351996976b38ffd793b9f2ca38d1cf530c554
alozor <nicolas-mery@neuf.fr>
2021-01-24 17:53:19 +0100

    feat: add forward propagation

--- scripts/main.py
+++ scripts/main.py
@@ -2,6 +2,7 @@
 Backpropagation from scratch
 """
 
+from math import exp
 from random import random, seed
 
 
@@ -20,10 +21,32 @@ def initialize_network(n_inputs, n_hidden, n_outputs):
     return network
 
 
+def activate(weights, inputs):
+    return sum(map(lambda w, i: w * i, weights[:-1], inputs)) + weights[-1]
+
+
+def transfer(activation):
+    return 1.0 / (1.0 + exp(-activation))
+
+
+def forward_propagate(network, row):
+    inputs = row
+    for layer in network:
+        new_inputs = []
+        for neuron in layer:
+            neuron["output"] = transfer(activate(neuron["weights"], inputs))
+            new_inputs.append(neuron["output"])
+        inputs = new_inputs
+    return inputs
+
+
 def main():
     seed(1)
-    network = initialize_network(1, 2, 1)
+    network = initialize_network(2, 1, 2)
     print(network)
+    row = [1, 0, None]
+    output = forward_propagate(network, row)
+    print(output)
 
 
 if __name__ == "__main__":
