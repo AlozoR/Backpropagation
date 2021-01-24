cccb9f8ce4c8f24f2b5e07d3b0b91a3ea41b7e6a
alozor <nicolas-mery@neuf.fr>
2021-01-24 16:52:11 +0100

    feat: add initialize network function

---
+++ scripts/main.py
@@ -0,0 +1,30 @@
+"""
+Backpropagation from scratch
+"""
+
+from random import random, seed
+
+
+def initialize_network(n_inputs, n_hidden, n_outputs):
+    network = list()
+    hidden_layer = [
+        {"weights": [random() for i in range(n_inputs + 1)]}
+        for i in range(n_hidden)
+    ]
+    network.append(hidden_layer)
+    output_layer = [
+        {"weights": [random() for i in range(n_hidden + 1)]}
+        for i in range(n_outputs)
+    ]
+    network.append(output_layer)
+    return network
+
+
+def main():
+    seed(1)
+    network = initialize_network(1, 2, 1)
+    print(network)
+
+
+if __name__ == "__main__":
+    main()
